5814b7deb78b745860d5af6434399bcfc27c5eee
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-02-28 20:50:03 +0700

    fixing margin and read click

--- js/home.js
+++ js/home.js
@@ -2,12 +2,11 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
 });
 
+//when "tandi sudah dibaca" got clicked
 $("#notification-dropdown").click(function (e) {
-    e.preventDefault();
-    e.stopPropagation();
-});
-
-//read notification function
-$(".read-notif").click(function (e) {
-    e.preventDefault();
+    if ($(e.target).hasClass("read-notif")) {
+        $(e.target).parent().parent().addClass("read");
+        e.preventDefault();
+        e.stopPropagation();
+    }
 });
