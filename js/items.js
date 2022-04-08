ab12d605323c3b6304f2b9c6da68e5e8a9c522fa
Gunawan Nur Ahmad <54525132+GunawanAhmad@users.noreply.github.com>
2022-04-08 18:57:21 +0700

    feedback 8/4/22

--- js/items.js
+++ js/items.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
     $(".select2").select2({
         tags: true,
     });
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip({ trigger: "hover" });
+    });
 });
 
 //clear search input
 $(".clear-icon-search").click(function (e) {
     $("#thumbnail_icon_search_input").val("");
+});
+
+//when "tandai sudah dibaca" got clicked
+$("#notification-dropdown").click(function (e) {
+    if ($(e.target).hasClass("read-notif")) {
+        $(e.target).parent().parent().addClass("read");
+        e.preventDefault();
+        e.stopPropagation();
+    }
 });
